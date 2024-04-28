e20daf5f265cd10292edaf965f81d7b95831c8ad
dimitrievvasile <dimi3ievasile@gmail.com>
2024-04-28 23:51:12 +0300

    homework_nr_15 END

---
+++ src/main/java/homework_nr_15/Car.java
@@ -0,0 +1,32 @@
+package homework_nr_15;
+
+public class Car implements Comparable<Car> {
+    private int yearMade;
+    private String name;
+
+    public Car(int yearMade, String name) {
+        this.yearMade = yearMade;
+        this.name = name;
+
+    }
+
+    public Integer getYearMade() {
+        return yearMade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return Integer.compare(o.yearMade, this.yearMade);
+    }
+
+    @Override
+    public String toString() {
+        return yearMade + " - " + name;
+    }
+}
+
+
