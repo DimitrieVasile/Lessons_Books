2b649872b2cdef3c0579197d865361cfab17ede6
vasiledimitriev <dimi3ievasile@gmail.com>
2024-05-05 22:55:49 +0300

    optimized Homework_nr_17

--- src/main/java/homework_nr_17/Main.java
+++ src/main/java/homework_nr_17/Main.java
@@ -4,19 +4,18 @@ public class Main {
     public static void main(String[] args) {
         AlphabetPrinter alphabetPrinter = new AlphabetPrinter();
 
-        Thread thread1 = new Thread(() -> alphabetPrinter.printLetters());
-        Thread thread2 = new Thread(() -> alphabetPrinter.printLetters());
+        Thread thread1 = new Thread(alphabetPrinter::printLetters);
+        Thread thread2 = new Thread(alphabetPrinter::printLetters);
 
         thread1.start();
-        thread2.start();
-
 
         try {
             Thread.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
         }
 
         thread1.interrupt();
+        thread2.start();
     }
 }
